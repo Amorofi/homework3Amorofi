c0b3fddcdd8d966b33b32f2789be143daa2ed76b
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-03 22:05:46 -0500

    Create model-authors.php

---
+++ model-authors.php
@@ -0,0 +1,15 @@
+<?php
+function selectAuthors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT authors_id, authors_name, birth_date, nationality FROM `authors`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
