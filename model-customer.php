291593697934d84da996291cd12d900fbc0820f1
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-03 22:43:50 -0500

    Update model-customer.php

--- model-customer.php
+++ model-customer.php
@@ -3,7 +3,7 @@ function selectCustomer() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT customer_id, customer_name, email ,phone FROM `customers`");
-        $stmt->execute();
+
         $result = $stmt->get_result();
         $conn->close();
         return $result;
