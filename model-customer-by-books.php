e8626fb718c6c2e6ebd56075f701ff23daf50e37
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 00:20:11 -0500

    Update model-customer-by-books.php

--- model-customer-by-books.php
+++ model-customer-by-books.php
@@ -3,7 +3,7 @@ function selectCustomerBybooks($cid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT b.book_id, title, publication_date, author_id, date, customer_id FROM `books` b join orders o on o.book_id = b.book_id where o.customer_id =?");
-        $stmt->bind_param("c", $cid);
+        $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
