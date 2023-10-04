be5b5c81995b8bdcd089ef958d34ff7446af7c62
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 01:10:04 -0500

    Update model-customer-with-books.php

--- model-customer-with-books.php
+++ model-customer-with-books.php
@@ -12,4 +12,21 @@ function selectBooks() {
         throw $e;
     }
 }
+
+function selectCustomerBybooks($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT b.book_id, title, publication_date, author_id, date, customer_id FROM `books` b join orders o on o.book_id = b.book_id where o.customer_id =?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
