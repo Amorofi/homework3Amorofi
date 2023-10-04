87351df9982f67c39c8d3f208d134eb0bd4e2e44
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 00:52:17 -0500

    Create view-order-by-customer.php

---
+++ view-order-by-customer.php
@@ -0,0 +1,31 @@
+<h1>Order by Customer</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Books Title</th>
+        <th>Publication date</th>
+        <th>Author ID</th>
+        <th>Date</th>
+        <th>Customer ID</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      while ($order = $orders->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $order['book_id']; ?></td>
+          <td><?php echo $order['title']; ?></td>
+          <td><?php echo $order['publication_date']; ?></td>
+          <td><?php echo $order['author_id']; ?></td>
+          <td><?php echo $order['date']; ?></td>
+          <td><?php echo $order['customer_id']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
