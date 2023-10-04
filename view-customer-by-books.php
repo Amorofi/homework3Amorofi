ca4ea69b3f59ac054faed203abc408440b3a2b45
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 00:16:06 -0500

    Update view-customer-by-books.php

--- view-customer-by-books.php
+++ view-customer-by-books.php
@@ -1,23 +1,27 @@
-<h1>Customers by books</h1>
+<h1>Customer by books</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
         <th>ID</th>
-        <th>Customers </th>
-        <th>Emails</th>
-        <th>Phone Number</th>
+        <th>Books Title</th>
+        <th>Publication date</th>
+        <th>Author ID</th>
+        <th>Date</th>
+        <th>Customer ID</th>
       </tr>
     </thead>
     <tbody>
       <?php
-      while ($customers = $customer->fetch_assoc()) {
+      while ($book = $books->fetch_assoc()) {
       ?>
         <tr>
-          <td><?php echo $customers['customer_id']; ?></td>
-          <td><?php echo $customers['customer_name']; ?></td>
-          <td><?php echo $customers['email']; ?></td>
-          <td><?php echo $customers['phone']; ?></td>
+          <td><?php echo $book['book_id']; ?></td>
+          <td><?php echo $book['title']; ?></td>
+          <td><?php echo $book['publication_date']; ?></td>
+          <td><?php echo $book['author_id']; ?></td>
+          <td><?php echo $book['date']; ?></td>
+          <td><?php echo $book['customer_id']; ?></td>
         </tr>
       <?php
       }
