290c62138e3cb7279749340315800fe2337a504c
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-03 22:34:01 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -4,7 +4,7 @@ require_once("model-customer.php");
 
 $pageTitle = "Customers";
 include "view-header.php";
-$customers = selectCustomers();
+$customers = selectCustomer();
 include "view-customer.php";
 include "view-footer.php";
 ?>
