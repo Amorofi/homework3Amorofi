c552581f5fafc14c792830c5278773430c64a10b
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 00:18:01 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -29,7 +29,7 @@
           <a class="nav-link" href="customer.php">Customers</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="customers-with-books.php">Customers with books</a>
+          <a class="nav-link" href="customer-by-books.php">Customers with books</a>
         </li>
 
       </ul>
