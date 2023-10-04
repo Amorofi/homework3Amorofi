7246e235de8f090da5dbdac112a7ab5f51f45b8f
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 00:13:13 -0500

    Create view-customer-by-books.php

---
+++ view-customer-by-books.php
@@ -0,0 +1,27 @@
+<h1>Customers by books</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Customers </th>
+        <th>Emails</th>
+        <th>Phone Number</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      while ($customers = $customer->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $customers['customer_id']; ?></td>
+          <td><?php echo $customers['customer_name']; ?></td>
+          <td><?php echo $customers['email']; ?></td>
+          <td><?php echo $customers['phone']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
