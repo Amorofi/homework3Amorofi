d7be002332335fcd0f71b37371b68ee14ac713c5
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-03 23:30:13 -0500

    Update books-by-author.php

--- books-by-author.php
+++ books-by-author.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-books.php");
+require_once("model-books-by-author.php");
 
-$pageTitle = "Books";
+$pageTitle = "Books by Author";
 include "view-header.php";
-$books = selectBooks();
-include "view-books.php";
+$books = selectBooksByAuthor($_GET['id']);
+include "view-books-by-author.php";
 include "view-footer.php";
 ?>
