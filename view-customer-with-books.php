bebf41eaf6f485e1ba58b35f6aa0c28a1c88fa70
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-04 01:35:57 -0500

    Update view-customer-with-books.php

--- view-customer-with-books.php
+++ view-customer-with-books.php
@@ -7,13 +7,17 @@ while ($book = $books->fetch_assoc()) {
             <div class="card-body">
                   <h5 class="card-title"><?php echo $book['title']; ?></h5>
                   <p class="card-text">
+                   <ul class="list-group">
 <?php
       $customer = selectCustomerByBooks($book['book_id'];);
       while ($customer = $customer->fetch_assoc()) {
 ?>
+       <li class="list-group-item"><?php echo $book['publication_date']; ?> - <?php echo $book['title']; ?> - ><?php echo $book['date']; ?> </li>
+
 <?php
       }
 ?>
+                   </ul>
           </p>
           <p class="card-text"><small class="text-body-secondary">publication: <?php echo $book['publication_date']; ?> </small></p>
        </div>
