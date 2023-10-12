9197548e1d74288789e4201928be7d543d5dcaf0
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-11 20:08:30 -0500

    Update model-order-by-customer.php

--- model-order-by-customer.php
+++ model-order-by-customer.php
@@ -2,7 +2,7 @@
 function selectOrderByCustomer($cid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT b.book_id, title, publication_date, author_id, date, customer_id FROM `books` b join orders o on o.book_id = b.book_id where o.book_id =?");
+        $stmt = $conn->prepare("SELECT b.book_id,order_id title, publication_date, author_id, date, customer_id FROM `books` b join orders o on o.book_id = b.book_id where o.book_id =?");
         $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
