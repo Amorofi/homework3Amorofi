bbb89696ea8ba4fb8522ccb535ee3b285fbf6750
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-11 22:57:07 -0500

    Update model-customer-with-books.php

--- model-customer-with-books.php
+++ model-customer-with-books.php
@@ -1,8 +1,8 @@
 <?php
-function selectBooks() {
+function selectCustomers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT book_id, title, publication_date, author_id FROM `books`");
+        $stmt = $conn->prepare("SELECT customer_id, customer_name, email ,phone FROM `customer`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -12,21 +12,4 @@ function selectBooks() {
         throw $e;
     }
 }
-
-function selectCustomerbybooks($iid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT b.book_id, title, publication_date, author_id, date, customer_id FROM `books` b join orders o on o.book_id = b.book_id where o.customer_id =?");
-        $stmt->bind_param("i", $iid);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-
 ?>
