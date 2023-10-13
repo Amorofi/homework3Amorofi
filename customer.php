4663f29e472a33cf6914f36240f2ed7ebddc2082
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-12 23:24:57 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -16,7 +16,7 @@ if (isset($_POST['actionType'])) {
       break;
 
         case "Delete":
-      if (deleteCustomers($_POST['cid])) {
+      if (deleteCustomers($_POST['cid'])) {
           echo '<div class="alert alert-success" role="alert"> Customer was deleted. </div>'; 
         }else {
           echo '<div class="alert alert-danger" role="alert"> Error try again. </div>';
