6be7d199e35bcea2f547cdc33987d0187c63f168
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-12 21:57:36 -0500

    Update model-customer.php

--- model-customer.php
+++ model-customer.php
@@ -12,4 +12,46 @@ function selectCustomers() {
         throw $e;
     }
 }
+
+function insertCustomers($cName, $cMail, $cNumber) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `customer` (`customer_name`, `email`, `phone`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cCustomer, $cInfo, $cNumber);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCustomers($cName, $cMail, $cNumber, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `customer` set  `customer_name` = ?, `email` = ?, `phone` = ? where customer_id = ?");
+        $stmt->bind_param("sssi", $cCustomer, $cInfo, $cNumber, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCustomers($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from customer where customer_id = ?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
