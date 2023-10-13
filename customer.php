40848f299819676b40ab6d02ffc31f6f7e895f75
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-12 23:09:08 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      insertCustomers($_POST['cName'], $_POST['cMail'], $_POST['cNumber']);
+      if (insertCustomers($_POST['cName'], $_POST['cMail'], $_POST['cNumber'])) {
+          echo '<div class="alert alert-success" role="alert"> Customer added </div>'; 
+        }else {
+          echo '<div class="alert alert-danger" role="alert"> </div>';
+        }
       break;
   }
 }
