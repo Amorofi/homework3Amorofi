f924af7d5bfc08c935d798823445c24e85706589
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-12 22:42:31 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -4,6 +4,14 @@ require_once("model-customer.php");
 
 $pageTitle = "Customer";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertCustomers($_POST['cName'], $_POST['cMail'],$_POST['cNumber'];
+    break;
+  }
+}
 $customer = selectCustomers();
 include "view-customer.php";
 include "view-footer.php";
