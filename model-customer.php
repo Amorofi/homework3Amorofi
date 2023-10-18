f0f9d45b180e5816dfde18ba4e9e04bbac61962e
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-18 13:56:03 -0500

    Update model-customer.php

--- model-customer.php
+++ model-customer.php
@@ -17,7 +17,7 @@ function insertCustomers($cName, $cMail, $cNumber) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `customer` (`customer_name`, `email`, `phone`) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $cCustomer, $cInfo, $cNumber);
+        $stmt->bind_param("sss", $cName, $cMail, $cNumber);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -31,7 +31,7 @@ function updateCustomers($cName, $cMail, $cNumber, $cid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("update `customer` set  `customer_name` = ?, `email` = ?, `phone` = ? where customer_id = ?");
-        $stmt->bind_param("sssi", $cCustomer, $cInfo, $cNumber, $cid);
+        $stmt->bind_param("sssi", $cName, $cMail, $cNumber, $cid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
