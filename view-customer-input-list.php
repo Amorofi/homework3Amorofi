19dc97e8beeeb74492f51b937fdcacb90263aed2
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-19 12:06:19 -0500

    Update view-customer-input-list.php

--- view-customer-input-list.php
+++ view-customer-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="cid" name="cid">
 <?php
 while($customerItem = $customerList->fetch_assoc()) {
+    $selText = "";
+    if($selectedcustomer == $customerItem['customer_id']) {
+        $selText = "selected";
+    }
 ?> 
-    <option value="<?php echo $customerItem['customer_id']; ?> " ><?php echo $customerItem['customer_name']; ?> </option>
+    <option value="<?php echo $customerItem['customer_id']; ?> " <?=$selText?> ><?php echo $customerItem['customer_name']; ?> </option>
 <?php
 }
 ?>
