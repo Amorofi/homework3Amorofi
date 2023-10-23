d3caadfde1094c817e84a4302782b65a07273715
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-23 14:03:29 -0500

    Update model-customer-with-books.php

--- model-customer-with-books.php
+++ model-customer-with-books.php
@@ -28,5 +28,74 @@ function selectCustomerbybooks($iid) {
     }
 }
 
+function selectCustomersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT customer_id, customer_name, FROM `customer` order by customer_name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectBooksForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT book_id, title, FROM `books` order by title");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertOrder($cid, $bid, $date) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `orders` (`customer_id`, `book_id`, `date`) VALUES (?, ?, ?)");
+        $stmt->bind_param("sss", $cid, $bid, $date);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateOrder($cid, $bid, $date, $oid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `orders` set  `customer_id` = ?, `book_id` = ?, `date` = ? where order_id = ?");
+        $stmt->bind_param("sssi", $cid, $bid, $date,$oid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteOrder($oid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from orders where order_id = ?");
+        $stmt->bind_param("i", $oid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
