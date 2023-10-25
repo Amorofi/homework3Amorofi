a84893ef996e65162a762dd68eaabb8819e41b6b
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-25 18:47:39 -0500

    Update model-customer-with-books.php

--- model-customer-with-books.php
+++ model-customer-with-books.php
@@ -11,8 +11,10 @@ function selectCustomers() {
         $conn->close();
         throw $e;
     }
-} 
+}
+?>
 
+<?php
 function selectCustomerbybooks($iid) {
     try {
         $conn = get_db_connection();
@@ -27,76 +29,4 @@ function selectCustomerbybooks($iid) {
         throw $e;
     }
 }
-
-function selectCustomersForInput() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT customer_id, customer_name, FROM `customer` order by customer_name");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-function selectBooksForInput() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT book_id, title, FROM `books` order by title");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-function insertOrder($cid, $bid, $ttitle, $pd, $aid, $ddate) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `orders` (`customer_id`, `book_id`, 'title', 'publication_date', 'author_id', 'date') VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sss", $cid, $bid, $ttitle, $pd, $aid, $ddate);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-function updateOrder($cid, $bid, $ttitle, $pd, $aid, $ddate, $oid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("update `orders` set  `customer_id` = ?, `book_id` = ?, 'title' = ?, 'publication_date' = ?, `date` = ? where order_id = ?");
-        $stmt->bind_param("sssi", $cid, $bid,$ttitle, $pd, $aid, $ddate, $oid);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-function deleteOrder($oid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("delete from orders where order_id = ?");
-        $stmt->bind_param("i", $oid);
-        $success = $stmt->execute();
-        $conn->close();
-        return $success;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-
-}
-
 ?>
