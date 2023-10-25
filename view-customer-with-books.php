73feef888d51112cc661ebf45e5bd36efae9df63
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-25 18:48:29 -0500

    Update view-customer-with-books.php

--- view-customer-with-books.php
+++ view-customer-with-books.php
@@ -1,65 +1,32 @@
-<div class = "row">
-  <div class="col">
-    <h1>Customers with books</h1>
-  </div>
- <div class="col-auto">
 <?php
-include "view-customer-with-books-newform.php";
-?>
- </div>
-
-</div>
-
-<div class="card-group">
-<?php
-while ($customers = $customer->fetch_assoc()) {
-?>
- <div class="card">
-   <div class="card-body">
-      <h5 class="card-title"><?php echo $customers['customer_name']; ?></h5>
-      <p class="card-text">
-      <ul class="list-group">
-
-        
-<?php
- $books = selectCustomerbybooks($customers['customer_id']);
- while ($book = $books->fetch_assoc()) {
-?> 
-    <li class="list-group-item">
-    <div class="row">
-      <div class="col">
-        <?php echo $book['title']; ?> - <?php echo $book['author_id']; ?> - <?php echo $book['publication_date']; ?> - <?php echo $book['date']; ?>
-      </div>
-      <div class="col-auto">
-<?php  
-include "view-customer-with-books-editform.php"
-?>
-   
-    </div>
-      <div class="col-auto">
-        <form method="post" action="">
-          <input type="hidden" name="oid" value="<?php echo $book['order_id']; ?>">
-          <input type="hidden" name="actionType" value="Delete">
-          <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-            </svg>
-          </button>
-        </form>
-      </div>
-    </div>
-    </li>
-  
-<?php
- }
-?>
-      </ul>
-      </p>
-      <p class="card-text"><small class="text-body-secondary">Email: <?php echo $customers['email']; ?></small></p>
-      </div>
- </div>
-<?php
+function selectCustomers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT customer_id, customer_name, email ,phone FROM `customer`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
 }
 ?>
-</div>
+
+<?php
+function selectCustomerbybooks($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT b.book_id, title, publication_date, author_id, order_id, date FROM `books` b join orders o on o.book_id = b.book_id where o.customer_id =? ");
+        $stmt->bind_param("i", $iid,);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
