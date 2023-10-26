9998c971a308c263b12b761bd11f8402181b0ea1
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-10-25 19:04:49 -0500

    Update customer-with-books.php

--- customer-with-books.php
+++ customer-with-books.php
@@ -3,7 +3,36 @@ require_once("util-db.php");
 require_once("model-customer-with-books.php");
 
 $pageTitle = "Customer with books";
-include "view-header.php";
+include "view-header.php"; 
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertOrders($_POST[`cid`], $_POST[`bid`],  $_POST['ttitle'], $_POST['pd'], $_POST['aid'], $_POST[`ddate`])) {
+          echo '<div class="alert alert-success" role="alert"> Customer was successfully added. </div>'; 
+        }else {
+          echo '<div class="alert alert-danger" role="alert"> Error try again. </div>';
+        }
+      break;
+
+        case "Edit":
+      if (updateOrders($_POST[`cid`], $_POST[`bid`],  $_POST['ttitle'], $_POST['pd'], $_POST['aid'], $_POST[`ddate`], $_POST['$oid'])) {
+          echo '<div class="alert alert-success" role="alert"> Customer was successfully edited. </div>'; 
+        }else {
+          echo '<div class="alert alert-danger" role="alert"> Error try again. </div>';
+        }
+      break;
+    
+        case "Delete":
+      if (deleteOrders($_POST['$oid'])) {
+          echo '<div class="alert alert-success" role="alert"> Customer was successfully deleted. </div>'; 
+        }else {
+          echo '<div class="alert alert-danger" role="alert"> Error try again. </div>';
+        }
+      break;
+  }
+}
+
 $customer = selectCustomers();
 include "view-customer-with-books.php";
 include "view-footer.php";
