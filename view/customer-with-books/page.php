ac4be13ff3f2f72337298377fc8f9c1b755d8994
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-11-15 00:37:51 -0600

    Update page.php

--- view/customer-with-books/page.php
+++ view/customer-with-books/page.php
@@ -3,6 +3,23 @@
 <?php
 while ($customers = $customer->fetch_assoc()) {
  include "customer.php";
+ ?>
+ <div class="card">
+   <div class="card-body">
+      <h5 class="card-title"><?php echo $customers['customer_name']; ?></h5>
+      <p class="card-text">
+      <ul class="list-group">
+<?php
+ $books = selectCustomerbybooks($customers['customer_id']);
+ while ($book = $books->fetch_assoc()) {
+ <?php
+ }
+?>
+      </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Email: <?php echo $customers['email']; ?></small></p>
+      </div>
+ </div>
 ?>     
   <li class="list-group-item"><?php echo $book['title']; ?> - <?php echo $book['author_id']; ?> - <?php echo $book['publication_date']; ?> - <?php echo $book['date']; ?></li>
 <?php
