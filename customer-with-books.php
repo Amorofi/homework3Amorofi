d003374872ff262cbfd37c4295b364108f99fe1b
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-11-15 00:16:07 -0600

    Update customer-with-books.php

--- customer-with-books.php
+++ customer-with-books.php
@@ -5,6 +5,6 @@ require_once("model/customer-with-books.php");
 $pageTitle = "Customer with books";
 include "view-header.php";
 $customer = selectCustomers();
-include "view-customer-with-books.php";
+include "view/customer-with-books/page.php";
 include "view-footer.php";
 ?>
