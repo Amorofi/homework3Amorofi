20eae120940d801f47d11c881134733b669fd274
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-11-15 00:30:12 -0600

    Update page.php

--- view/customer-with-books/page.php
+++ view/customer-with-books/page.php
@@ -3,6 +3,9 @@
 <?php
 while ($customers = $customer->fetch_assoc()) {
  include "customer.php";
+?>     
+  <li class="list-group-item"><?php echo $book['title']; ?> - <?php echo $book['author_id']; ?> - <?php echo $book['publication_date']; ?> - <?php echo $book['date']; ?></li>
+<?php
 
 }
 ?>
