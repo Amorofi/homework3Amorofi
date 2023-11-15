9add6890e354b6577a786b8e8dd48f0f8003ff38
Amorofi <123018699+Amorofi@users.noreply.github.com>
2023-11-15 00:27:22 -0600

    Create book.php

---
+++ view/customer-with-books/book.php
@@ -0,0 +1,5 @@
+?>     
+  <li class="list-group-item"><?php echo $book['title']; ?> - <?php echo $book['author_id']; ?> - <?php echo $book['publication_date']; ?> - <?php echo $book['date']; ?></li>
+<?php
+ }
+?>
